539146d8305c2e60bf173eb2c1b89bcf2c48450c
Carl Boettiger <cboettig@gmail.com>
2017-12-28 16:47:44 -0800

    clear environment to free space as we go

--- appendix/solve_pomdp.R
+++ appendix/solve_pomdp.R
@@ -1,10 +1,15 @@
 
 rmarkdown::render("appendix/appendix_harvestfirst.Rmd")
+rm(list=ls())
 rmarkdown::render("appendix/appendixB.Rmd")
+rm(list=ls())
 #rmarkdown::render("appendix/appendix.Rmd")      ## running on chameleon
-
+rm(list=ls())
 
 ## super slow
 #rmarkdown::render("appendix/appendix_99.Rmd")   ## running on 24-core
+rm(list=ls())
 rmarkdown::render("appendix/appendix_harvestfirst_99.Rmd")
+rm(list=ls())
 rmarkdown::render("appendix/appendixB_99.Rmd")
+rm(list=ls())
